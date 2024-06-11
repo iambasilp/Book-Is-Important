f626819b09ed188c0844d77a800ef804648638c0
iambasilp <iambasilp@gmail.com>
2024-06-11 17:13:11 +0530

    updated

--- src/components/ReadNowCard.jsx
+++ src/components/ReadNowCard.jsx
@@ -1,23 +1,36 @@
-const ReadNowCard = ({ img, title, description, buttonUrl, isRead, onAction }) => {
+import Tool from "./Tool";
+const ReadNowCard = ({
+  img,
+  title,
+  description,
+  buttonUrl,
+  isRead,
+  onAction,
+}) => {
   return (
-    <a href={buttonUrl} className="carousel-item" onClick={onAction}>
-      <div className="card w-96 bg-base-100 shadow-xl hover:bg-blue-600 hover:text-white transition-all">
-        <div className="object-cover">
-          <img src={img} alt="card image" className="w-full rounded-box" />
-        </div>
+    <Tool>
+      <a href={buttonUrl} className="carousel-item" onClick={onAction}>
+        <div className="card w-96 bg-base-100 shadow-xl hover:bg-blue-600 hover:text-white transition-all">
+          <div className="object-cover">
+            <img src={img} alt="card image" className="w-full rounded-box" />
+          </div>
 
-        <div className="card-body ">
-          <h2 className="card-title">
-            {title}
-          </h2>
-          <p>{description}</p>
-          <div className="card-actions justify-end">
-            <div className={`badge badge-outline border border-none p-3 ${isRead ? "bg-green-700 text-white" : "bg-red-700 text-white"}`}>{isRead ? `Read` : "Unread"}</div>
-            
+          <div className="card-body ">
+            <h2 className="card-title">{title}</h2>
+            <p>{description}</p>
+            <div className="card-actions justify-end">
+              <div
+                className={`badge badge-outline border border-none p-3 ${
+                  isRead ? "bg-green-700 text-white" : "bg-red-700 text-white"
+                }`}
+              >
+                {isRead ? `Read` : "Unread"}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </a>
+      </a>
+    </Tool>
   );
 };
 
---
+++ src/components/Tool.jsx
@@ -0,0 +1,11 @@
+import React,{Children} from 'react'
+
+const ReadCard = (props) => {
+  return (
+    <div>
+       {props.children}
+    </div>
+  )
+}
+
+export default ReadCard

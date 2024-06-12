aaaecd84da47b2eb35825026984a995d6b283445
iambasilp <iambasilp@gmail.com>
2024-06-12 14:03:40 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import ReadNow from "./components/ReadNow";
+import BenifitsOfReading from "./components/BenifitsOfReading";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Home />
       <About />
       <ReadNow />
+      <BenifitsOfReading />
     </div>
   );
 };
